5902cf094979218ef2606980c9d51ddde03a53eb
flashburst <82953782+flashburst@users.noreply.github.com>
2022-11-30 19:57:32 +0530

    Updated mock data

--- services/blog.js
+++ services/blog.js
@@ -2,6 +2,37 @@ import slugify from 'slugify'
 import { storeLocally } from './io/download'
 import { mockData } from './_mock_'
 
+export const getLatestBlogPosts = async () => {
+  try {
+    //   const dataStr = await request.get(`${API_URL_PREFIX}articles?limit=3`)
+    //   const data = JSON.parse(dataStr)
+    const data = mockData.articleList
+
+    const result = await Promise.allSettled(data.docs.slice(0, 10).map(async (doc) => {
+      return {
+        id: doc.id,
+        title: doc.title,
+        image: await storeLocally(`${process.env.FILE_URL_PREFIX}${doc.cover.filename}`, 'blog-images'),
+        // image: `/${doc.cover.filename}`,
+        slug: `https://blog.neptunemutual.com/${slugify(doc.title, {
+            lower: true,
+            trim: true,
+            strict: true
+        })}`,
+        intro: doc.intro.replace('&hellip;', ''),
+        date: doc.updatedAt || doc.createdAt,
+        tags: doc.tags.map((tag) => tag.name).slice(0, 1)
+      }
+    }))
+
+    return result.map(x => x.value)
+  } catch (error) {
+    console.error(error)
+  }
+
+  return []
+}
+
 export const getBlogPosts = async () => {
   try {
     //   const dataStr = await request.get(`${API_URL_PREFIX}articles?limit=3`)
