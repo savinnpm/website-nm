f1d40185faa9aeeb06c7c8a37f6d95a71a464953
flashburst <82953782+flashburst@users.noreply.github.com>
2022-12-02 14:16:05 +0530

    Added icons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -19,5 +19,35 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render () {
+    return (
+      <Html>
+        <Head>
+          <link rel='apple-touch-icon' sizes='57x57' href='/assets/images/meta/icons/apple-icon-57x57.png' />
+          <link rel='apple-touch-icon' sizes='60x60' href='/assets/images/meta/icons/apple-icon-60x60.png' />
+          <link rel='apple-touch-icon' sizes='72x72' href='/assets/images/meta/icons/apple-icon-72x72.png' />
+          <link rel='apple-touch-icon' sizes='76x76' href='/assets/images/meta/icons/apple-icon-76x76.png' />
+          <link rel='apple-touch-icon' sizes='114x114' href='/assets/images/meta/icons/apple-icon-114x114.png' />
+          <link rel='apple-touch-icon' sizes='120x120' href='/assets/images/meta/icons/apple-icon-120x120.png' />
+          <link rel='apple-touch-icon' sizes='144x144' href='/assets/images/meta/icons/apple-icon-144x144.png' />
+          <link rel='apple-touch-icon' sizes='152x152' href='/assets/images/meta/icons/apple-icon-152x152.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/assets/images/meta/icons/apple-icon-180x180.png' />
+          <link rel='icon' type='image/png' sizes='192x192' href='/assets/images/meta/icons/android-icon-192x192.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/assets/images/meta/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='96x96' href='/assets/images/meta/favicons/favicon-96x96.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/assets/images/meta/favicons/favicon-16x16.png' />
+          <link rel='manifest' href='/manifest.json' />
+          <meta name='msapplication-TileColor' content='#01052D' />
+          <meta name='msapplication-TileImage' content='/assets/images/meta/icons/ms-icon-144x144.png' />
+          <meta name='theme-color' content='#01052D' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
