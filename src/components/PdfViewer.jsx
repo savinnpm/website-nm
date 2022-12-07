8303806f59af74a4fcf904e1fba13cdc7ae84493
flashburst <82953782+flashburst@users.noreply.github.com>
2022-12-07 12:11:57 +0530

    Fixed audit pdf

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -1,18 +1,40 @@
 import styled from 'styled-components'
 import { colors } from '../../styles/colors'
+import { typography } from '../../styles/typography'
+import { utils } from '../../styles/utils'
+import { Button } from './Button'
 
 export const PdfViewer = ({ url }) => {
   return (
     <Container>
-      <StyledPdfEmbed data={`${url}#view=FitH&toolbar=0&toolbar=0&navpanes=0&scrollbar=0`} type='application/pdf' width='100%'>
-        <p>Your web browser doesn't have a PDF plugin. Instead you can <a href={url}>click here to download the PDF file.</a></p>
-      </StyledPdfEmbed>
+      <InnerContainer>
+        <StyledPdfEmbed data={`${url}#view=FitH&toolbar=0&toolbar=0&navpanes=0&scrollbar=0`} type='application/pdf' width='100%'>
+          <TextContainer>
+            <Heading>Note</Heading>
+            <SupportingText>Your web browser doesn't have a PDF plugin. Instead you can click below to download the PDF file.</SupportingText>
+          </TextContainer>
+
+          <ButtonContainer>
+            <Button
+              href={url}
+              as='a'
+              hierarchy='primary'
+              size='xl'
+              iconLeading
+              iconVariant='download-01'
+            >
+              Download File
+            </Button>
+          </ButtonContainer>
+        </StyledPdfEmbed>
+      </InnerContainer>
     </Container>
   )
 }
 
 const Container = styled.div`
   background-color: ${colors.gray['600']};
+  color: ${colors.white};
   padding-top: 96px;
   padding-bottom: 96px;
 
@@ -22,6 +44,10 @@ const Container = styled.div`
   }
 `
 
+const InnerContainer = styled.div`
+  ${utils.fullWidthContainer};
+`
+
 const StyledPdfEmbed = styled.object`
   display: block;
   max-width: 768px;
@@ -29,5 +55,44 @@ const StyledPdfEmbed = styled.object`
   margin-right: auto;
 
   height: 90vh;
-  min-height: 600px
+  min-height: 600px;
+`
+
+const TextContainer = styled.div`
+  max-width: 768px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Heading = styled.h2`
+  ${typography.styles.displayMd};
+  ${typography.weights.semibold};
+
+  @media (max-width: 768px) {
+    ${typography.styles.displaySm};
+    ${typography.weights.semibold};
+  }
+`
+
+const SupportingText = styled.p`
+  margin-top: 20px;
+
+  ${typography.styles.textXl};
+  ${typography.weights.regular};
+
+  @media (max-width: 768px) {
+    margin-top: 16px;
+    ${typography.styles.textLg};
+    ${typography.weights.regular};
+  }
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 20px;
+
+  @media (max-width: 768px) {
+    margin-top: 16px;
+  }
 `
