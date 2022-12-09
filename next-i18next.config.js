6acf6f00582dd9f228a2ecadd47d5641a5220484
Mark Adriano <mark.adriano15@gmail.com>
2022-12-09 18:23:16 +0800

    Add the other languuages to next-i18next.config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,8 @@
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zh', 'fr', 'de', 'id', 'it', 'ja', 'ko', process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'development' ? 'test' : undefined].filter(Boolean)
+    locales: ['en', 'zh', 'fr', 'de', 'id', 'it', 'ja', 'ko', 'ru', 'es', 'el', 'tr', 'vi', process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'development' ? 'test' : undefined].filter(Boolean)
   },
   localePath: './public/locales'
 }
