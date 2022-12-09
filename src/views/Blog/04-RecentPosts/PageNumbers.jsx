cf946276e0595924a1fe9252053f43a64adbf211
acharyarupak391 <acharyarupak391@gmail.com>
2022-12-09 19:12:03 +0545

    comments added in PageNumbers component for ease of understanding

--- src/views/Blog/04-RecentPosts/PageNumbers.jsx
+++ src/views/Blog/04-RecentPosts/PageNumbers.jsx
@@ -8,29 +8,46 @@ const PAGINATION_INDEX = 3
 export const PageNumbers = ({ pages, setPage, page }) => {
   const isMobile = useMediaQuery('(max-width: 768px)')
 
+  // start slice begins with 0 and has page 0, 1 & 2
   const startSliceBegin = 0
+
+  // end slice begins with the third last page (3rd last, 2nd last & the last page)
   const endSliceBegin = pages.length - PAGINATION_INDEX
 
   let middleSlice = []
+
+  // if current page is anywhere between(including) 3rd & 3rd last page,
+  // only then we have the middle slice else only start & end slice
   if ((endSliceBegin >= page) &&
     (page >= (startSliceBegin + PAGINATION_INDEX - 1))) {
+    // if current page is anywhere between(including) 5th & 5th last page,
+    // then middle slice will have 3 pages
+    // (current page - 1, current page, current page + 1)
     if ((page >= startSliceBegin + PAGINATION_INDEX + 1) &&
-      (page <= endSliceBegin - 2)) {
+        (page <= endSliceBegin - 2)) {
       middleSlice = pages.slice(page - 1, page + 2)
     }
 
+    // if current page is the 3rd page,
+    // then middle slice will have only 1 page, the 4th page
     if (page === startSliceBegin + PAGINATION_INDEX - 1) {
       middleSlice = pages.slice(page + 1, page + 2)
     }
 
+    // if current page is the 4th page,
+    // then middle slice will have 2 pages, the 4th page & 5th page
     if (page === startSliceBegin + PAGINATION_INDEX) {
       middleSlice = pages.slice(page, page + 2)
     }
 
+    // if current page is the 3rd last page,
+    // then middle slice will have only 1 page, the 4th last page
     if (page === endSliceBegin) {
       middleSlice = pages.slice(page - 1, page)
     }
 
+    // if current page is the 4th last page,
+    // then middle slice will have 2 pages, the 5th last page & 4th last page
     if (page === endSliceBegin - 1) {
       middleSlice = pages.slice(page - 1, page + 1)
     }
