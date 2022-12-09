72c4fabc45846a76b57b31e5bb0e8383028b966a
sansiven <sansiven@gmail.com>
2022-12-09 14:01:04 +0545

    updated images for partners and protocols

--- src/views/Home/02-SecuringApps/data.js
+++ src/views/Home/02-SecuringApps/data.js
@@ -2,8 +2,8 @@ export const data = [
   { name: 'Aave', src: '/assets/images/securing-apps/aave.webp' },
   { name: 'Balancer', src: '/assets/images/securing-apps/balancer.webp' },
   { name: 'Gnosis Safe', src: '/assets/images/securing-apps/gnosissafe.webp' },
-  { name: 'Sushi Swap', src: '/assets/images/securing-apps/sushiswap.webp' },
-  { name: '1 inch', src: '/assets/images/securing-apps/1inch.webp' },
   { name: 'Maker DAO', src: '/assets/images/securing-apps/makerdao.webp' },
-  { name: 'Binance', src: '/assets/images/securing-apps/binance.webp' }
+  { name: 'Binance', src: '/assets/images/securing-apps/binance.webp' },
+  { name: 'Uniswap', src: '/assets/images/securing-apps/uniswap.webp' },
+  { name: 'Curve', src: '/assets/images/securing-apps/curve.webp' }
 ]
