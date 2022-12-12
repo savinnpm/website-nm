d7605b7d6e37362378b635cf034f13fe6ba982ab
flashburst <82953782+flashburst@users.noreply.github.com>
2022-12-12 06:27:48 +0000

    Fixed typo

--- services/api/blog.js
+++ services/api/blog.js
@@ -105,7 +105,7 @@ export const getSinglePost = async (slug) => {
         title: match.meta.title,
         description: match.meta.description,
         image: {
-          src: await helpers.storeOgImage(match?.meta?.image?.filename || match.cover.filename, 'og-images'),
+          src: await helpers.storeOgImage(match?.meta?.image?.filename || match.cover.filename),
           alt: helpers.getOgImageAlt(match?.meta?.image?.alt)
         }
       },
