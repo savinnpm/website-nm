839983d5cd38a45adb7f4abe89f1492a9752ca22
sansiven <sansiven@gmail.com>
2022-12-12 09:49:08 +0545

    rebased with main

---
+++ src/components/Button/SecondaryGrayButton/gap.js
@@ -0,0 +1,25 @@
+import { css } from 'styled-components'
+
+const getGapLg = (size) => {
+  if (size === '2xl') {
+    return '12px'
+  }
+
+  return '8px'
+}
+
+const getGapMd = (size) => {
+  if (size === '2xl') {
+    return '8px'
+  }
+
+  return '8px'
+}
+
+export const gap = css`
+  gap: ${(props) => getGapLg(props.size)};
+
+  @media (max-width: 768px) {
+    gap: ${props => getGapMd(props.size)}; 
+  }
+`
