b7c28b4de959bddeb31f104fd49fd7c2525763ac
flashburst <82953782+flashburst@users.noreply.github.com>
2022-12-15 14:17:51 +0530

    Update download script

--- services/io/download.js
+++ services/io/download.js
@@ -3,28 +3,35 @@ import https from 'https'
 import path from 'path'
 import { ensureDirectory, exists } from '.'
 
-const download = function (url, dest, cb) {
-  const request = https.get(url, function (response) {
-    if (response.statusCode !== 200) {
-      return
-    }
+const download = function (url, dest) {
+  return new Promise((resolve, reject) => {
+    const request = https.get(url, function (response) {
+      if (response.statusCode !== 200) {
+        reject(Error('Failed with status: ' + response.statusCode))
+        return
+      }
 
-    const file = fs.createWriteStream(dest)
-    response.pipe(file)
+      const file = fs.createWriteStream(dest)
+      response.pipe(file)
 
-    file.on('finish', function () {
-      file.close(() => cb(null)) // close() is async, call cb after close completes.
-    })
-  })
-
-  request.on('error', function (err) { // Handle errors
-    // Delete the file async. (But we don't check the result)
-    fs.unlink(dest, (err) => {
-      console.warn('Could not delete')
-      cb(err)
+      file.on('finish', function () {
+        file.close(() => {}) // close() is async, call cb after close completes.
+        resolve()
+      })
     })
 
-    if (cb) cb(err)
+    request.on('error', function (err) { // Handle errors
+      // Delete the file async. (But we don't check the result)
+
+      console.log('Failed downloading, deleting unfinished file...')
+
+      fs.unlink(dest, (err) => {
+        console.log('Could not delete file...', dest)
+        reject(err)
+      })
+
+      reject(err)
+    })
   })
 }
 
@@ -43,14 +50,12 @@ export const storeLocally = async (url, folder = 'images') => {
     return '/' + publicPath
   }
 
-  download(url, storedAt, (err) => {
-    if (err) {
-      console.log('error downloading: ', filename)
-      return
-    }
-
-    console.log('downloaded: ', publicPath)
-  })
+  try {
+    await download(url, storedAt)
+  } catch (error) {
+    // Swallow
+    console.error(error)
+  }
 
   return '/' + publicPath
 }
