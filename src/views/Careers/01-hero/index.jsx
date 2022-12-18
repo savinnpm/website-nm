6c733bc045848ad51dff05b5f8a4efad8f3bfb6a
binodnpm <tokyo@neptunemutual.com>
2022-12-19 01:13:17 +0545

    Miscellaneous Refactors
    - Updated `We’re Hiring` badge to become blue for consistency
    - Fixed a couple of texts on contact us page

--- src/views/Careers/01-hero/index.jsx
+++ src/views/Careers/01-hero/index.jsx
@@ -57,8 +57,8 @@ const Badge = styled.div`
   display: inline-flex;
   ${typography.styles.textSm};
   ${typography.weights.medium};
-  color: ${props => props.theme.isLightMode ? colors.success[700] : colors.success[400]};
-  background: ${props => props.theme.isLightMode ? colors.success[50] : colors.gray[700]};
+  color: ${props => props.theme.isLightMode ? colors['blue-dark'][700] : colors['blue-dark'][50]};
+  background: ${props => props.theme.isLightMode ? colors['blue-dark'][100] : colors['blue-dark'][700]};
   padding: 2px 10px;
   border-radius: 16px;
 `
