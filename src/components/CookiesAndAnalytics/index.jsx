d978af5cf0a5a76ede0f69054a4ad9b46131389a
savinnpm <95403423+savinnpm@users.noreply.github.com>
2022-12-19 13:47:55 +0545

    add clarity tracking (#127)
    * add clarity to cokkies and analytics
    
    * update conditional rendering
    
    Co-authored-by: sansiven <sansiven@gmail.com>

--- src/components/CookiesAndAnalytics/index.jsx
+++ src/components/CookiesAndAnalytics/index.jsx
@@ -1,6 +1,7 @@
 import { ConsentPopup } from '../ConsentPopup'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { GoogleAnalytics } from './GoogleAnalytics'
+import { ClarityAnalytics } from './Clarity'
 
 export const CookiesAndAnalytics = () => {
   const [accepted, setAccepted] = useLocalStorage('npm-cookies-accepted', undefined)
@@ -9,6 +10,7 @@ export const CookiesAndAnalytics = () => {
   return (
     <>
       <GoogleAnalytics accepted={accepted} />
+      <ClarityAnalytics accepted={accepted} />
       {!sawPopup && <ConsentPopup setAccepted={setAccepted} />}
     </>
   )
