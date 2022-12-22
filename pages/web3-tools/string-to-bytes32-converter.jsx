b419ccb25c5ea43a617d905574ebce8e9aa8bc81
sansiven <sansiven@gmail.com>
2022-12-22 08:47:52 +0545

    update meta tags for new page

--- pages/web3-tools/string-to-bytes32-converter.jsx
+++ pages/web3-tools/string-to-bytes32-converter.jsx
@@ -1,12 +1,33 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router.js'
+import { useTranslation } from 'react-i18next'
+import { getFQDN } from '../../src/helpers/index.js'
 import { ToolsDetail } from '../../src/views/ToolsDetail/index.jsx'
 
 export default function Web3Pages () {
+  const router = useRouter()
+  const { t } = useTranslation('web3'
+  )
   return (
     <>
       <Head>
-        <title>String to Bytes32 converter</title>
+        <title>{t('META_TITLE')}</title>
+        <meta name='description' content={t('META_DESCRIPTION')} />
         <link rel='icon' href='/favicon.ico' />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={t('META_OG_TITLE')} />
+        <meta property='og:description' content={t('META_OG_DESCRIPTION')} />
+        <meta property='og:image' content={getFQDN(t('META_OG_IMAGE'))} />
+        <meta property='og:locale' content={router.locale} />
+        <meta property='og:url' content={router.asPath} />
+        <meta property='twitter:site' content='@neptunemutual' />
+        <meta property='twitter:creator' content='@neptunemutual' />
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:description' content={t('META_TWITTER_DESCRIPTION')} />
+        <meta property='twitter:title' content={t('META_TWITTER_TITLE')} />
+        <meta property='twitter:image' content={getFQDN(t('META_TWITTER_IMAGE'))} />
+        <meta property='twitter:image:alt' content={t('META_TWITTER_IMAGE_ALT')} />
 
       </Head>
 
