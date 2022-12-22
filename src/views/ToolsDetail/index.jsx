478dcc14635e90eb24506c3205edc891e3ed9982
savinnpm <95403423+savinnpm@users.noreply.github.com>
2022-12-22 11:22:04 +0545

    update meta tags for string to bytes32 converter page (#147)
    * update meta tags
    
    * get videos and pages to populate footer
    
    * update filenamme
    
    * add newsletter form
    
    Co-authored-by: sansiven <sansiven@gmail.com>

--- src/views/ToolsDetail/index.jsx
+++ src/views/ToolsDetail/index.jsx
@@ -1,9 +1,11 @@
+import { NewsletterSignupForm } from '../../components/NewsletterSignupForm'
 import { Hero } from './01-Hero'
 
 export const ToolsDetail = () => {
   return (
     <>
       <Hero />
+      <NewsletterSignupForm paddingTop />
     </>
   )
 }
