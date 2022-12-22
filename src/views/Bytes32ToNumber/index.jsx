0b907fc8b565bfadf258762504bb801b7b29d8ef
sansiven <sansiven@gmail.com>
2022-12-22 16:46:33 +0545

    add bytes32 to number header, footer and newsletter

---
+++ src/views/Bytes32ToNumber/index.jsx
@@ -0,0 +1,11 @@
+import { NewsletterSignupForm } from '../../components/NewsletterSignupForm'
+import { Hero } from './01-Hero'
+
+export const BytesToNumber = () => {
+  return (
+    <>
+      <Hero />
+      <NewsletterSignupForm paddingTop />
+    </>
+  )
+}
